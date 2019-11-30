d66f48f2b5d26c7ab08cd14934db82a34792ec3d
David-117 <50374125+David-117@users.noreply.github.com>
2019-11-30 13:50:40 -0600

    Add files via upload

---
+++ proyecto.sql
@@ -0,0 +1,31 @@
+
+create Database Edificios;
+
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('fernando Romero','Soumaya','1994','Conserva resguarda e investiga la colección de arte ','19.4406926','-99.2047001');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Juan O Gorman','Casas Azules','1939','19.4406926','-99.2047001');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Alberto Kalach','Galeria Kurimanzutto','1999','19.411691','-99.187702');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Juan O Gorman','Casas Azules','1939','19.4406926','-99.2047001');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Juan O Gorman','Casas Azules','1939','19.4406926','-99.2047001');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Juan O Gorman','Casas Azules','1939','19.4406926','-99.2047001');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Juan O Gorman','Casas Azules','1939','19.4406926','-99.2047001');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Juan O Gorman','Casas Azules','1939','19.4406926','-99.2047001');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Juan O Gorman','Casas Azules','1939','19.4406926','-99.2047001');
+Insert into('nombre_arquitecto','nombre_edificio','descripcion','año','delegacion','latitud','longitud') Values ('Juan O Gorman','Casas Azules','1939','19.4406926','-99.2047001');
+
+
+
+
+
+
+CREATE TABLE Edificios(
+nombre_arquitecto varchar(50),
+nombre_edificio varchar(50),
+descripcion varchar(60),
+año datetime NOT NULL,
+delegacion varchar(50),
+latitud varchar(25),
+longitud varchar(25)
+);
+
+
+
