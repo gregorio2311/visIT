0b10c949125b352ee9818fab46c9216785d60058
David-117 <50374125+David-117@users.noreply.github.com>
2019-11-30 17:47:10 -0600

    Update proyecto.sql

--- proyecto.sql
+++ proyecto.sql
@@ -1,14 +1,14 @@
 
 create Database servicios;
 
-Insert into('nombre_servicio','descripcion','delegacion','horario') Values ('gas','Servicio de gas estacionario','Coyoacan','lunes,miercoles,viernes');
-Insert into('nombre_servicio','descripcion','delegacion','horario') Values ('agua','servicio de agua','Cuauhtemoc','lunes,miercoles,viernes');
-Insert into('nombre_servicio','descripcion','delegacion','horario') Values ('tamales','venta de tamales','iztapalapa','lunez,miercoles,viernes');
-Insert into('nombre_servicio','descripcion','delegacion','horario') Values ( 'helados','venta de paletas y helados','benito juarez','sabados,domingos')
-Insert into('nombre_servicio','descripcion','delegacion','horario') Values ('tacos de canasta','venta de tacos','Cuauhtemoc','martes,jueves');
-Insert into('nombre_servicio','descripcion','delegacion','horario') Values ('camotes','venta de camotes','iztacalco','miercoles,viernes');
-Insert into('nombre_servicio','descripcion','delegacion','horario') Values ('pan','venta de pan de dulce','Cuauhtemoc','lunes,miercoles,viernes');
-Insert into('nombre_servicio','descripcion','delegacion','horario') Values ('elotes','venta de elotes y esquites','Benito Juarez','miercoles,viernes');
+Insert into('nombre_servicio','descripcion','delegacion','horario','imagenurl') Values ('gas','Servicio de gas estacionario','Coyoacan','lunes,miercoles,viernes', 'https://www.isentinel.com.mx/wp-content/uploads/2018/10/como-evito-que-me-roben-gas.jpg');
+Insert into('nombre_servicio','descripcion','delegacion','horario','imagenurl') Values ('agua','servicio de agua','Cuauhtemoc','lunes,miercoles,viernes','https://media.metrolatam.com/2018/10/18/aguacdmx-6454a7d4195ee914c4031879ad3ddd2b-600x400.jpg');
+Insert into('nombre_servicio','descripcion','delegacion','horario','imagenurl') Values ('tamales','venta de tamales','iztapalapa','lunez,miercoles,viernes','http://images.vendobara.com/anuncios_detalle/881bf88ba73a312111f457f42b1924cf.jpg');
+Insert into('nombre_servicio','descripcion','delegacion','horario','imagenurl') Values ('helados','venta de paletas y helados','benito juarez','sabados,domingos','https://cdn-5befa143f911c80db489a19d.closte.com/wp-content/uploads/2019/05/michoacana-nueva-santa-maria-slider.jpg')
+Insert into('nombre_servicio','descripcion','delegacion','horario','imagenurl') Values ('tacos de canasta','venta de tacos','Cuauhtemoc','martes,jueves','https://dam.cocinafacil.com.mx/wp-content/uploads/2019/06/tacos-canasta-frijoles-chorizo.jpg');
+Insert into('nombre_servicio','descripcion','delegacion','horario','imagenurl') Values ('camotes','venta de camotes','iztacalco','miercoles,viernes','https://iminox.org.mx/wp-content/uploads/pdfblog/175.9.jpg');
+Insert into('nombre_servicio','descripcion','delegacion','horario','imagenurl') Values ('pan','venta de pan de dulce','Cuauhtemoc','lunes,miercoles,viernes','https://www.mexicodesconocido.com.mx/wp-content/uploads/2010/06/pan-mexicano.jpg');
+Insert into('nombre_servicio','descripcion','delegacion','horario','imagenurl') Values ('elotes','venta de elotes y esquites','Benito Juarez','miercoles,viernes','https://cdn.quinto-poder.mx/wp-content/uploads/2019/03/7-elostes-mortales.jpg');
 
 
 CREATE TABLE servicios(
